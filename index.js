7d21c8899e774b855dfaba3f5e292e6fff8f5848
Fancy Stacks <fancystacks@MacBook-Pro.local>
2020-10-27 00:39:15 -0600

    getting JS started 🎮

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from './setup';
+
+const gameGrid = document.querySelector('#game');
+const scoreBox = document.querySelector('#score');
+const startButton = document.querySelector('#start-button');
