8d5a3d9b07c56586e72d753bf3a0e69654af28b7
Fancy Stacks <fancystacks@MacBook-Pro.local>
2020-10-27 00:55:15 -0600

    GameBoard Class 🎮

---
+++ GameBoard.js
@@ -0,0 +1,9 @@
+import { GRID_SIZE, CELL_SIZE, OBJECT_TYPE, CLASS_LIST } from './setup';
+
+class GameBoard {
+    constructor(DOMGrid) {
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid = DOMGrid;
+    }
+}
