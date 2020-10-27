0a1ef387e3e9fed51445f9e44b1dd1ee52484f65
Fancy Stacks <fancystacks@MacBook-Pro.local>
2020-10-27 02:12:27 -0600

    directions 🎮

--- Pacman.js
+++ Pacman.js
@@ -1,1 +1,54 @@
 import { OBJECT_TYPE, DIRECTIONS } from './setup';
+
+class Pacman {
+    constructor(speed, startPosition) {
+        this.position = startPosition;
+        this.speed = speed;
+        this.dir = null;
+        this.timer = 0;
+        this.powerPill = false;
+        this.rotation = true;
+    }
+
+    shouldMove() {
+        if (!this.dir) return false;
+
+        if (this.timer === this.speed) {
+            this.timer = 0;
+            return true;
+        }
+        this.timer++;
+    }
+    getNextMove(objectExists) {
+        let nextMovePosition = this.position + this.dir.movement;
+
+        if (
+            objectExists(nextMovePosition, OBJECT_TYPE.WALL) || 
+            objectExists(nextMovePosition, OBJECT_TYPE.GHOSTLAIR)
+        ) {
+            nextMovePosition = this.position;
+        }
+        return { nextMovePosition, direction: this.dir };
+    }
+
+    makeMove() {
+        const classesToRemove = [OBJECT_TYPE.PACMAN];
+        const classesToAdd = [OBJECT_TYPE.PACMAN];
+
+        return { classesToRemove, classesToAdd}
+    }
+
+    setNewPosition(nextMovePosition) {
+        this.position = nextMovePosition;
+    }
+
+    handleKeyInput(event, objectExists) {
+        let dir;
+
+        if (event.keyCode >= 37 && event.keyCode <= 40) {
+            dir = DIRECTIONS[event.key]
+        } else {
+            return;
+        }
+    }
+}
