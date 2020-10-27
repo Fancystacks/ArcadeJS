88cdf99314fcbf948b079e3e13f170f707e7a984
Fancy Stacks <fancystacks@MacBook-Pro.local>
2020-10-27 01:22:57 -0600

    set up methods 🎮

--- GameBoard.js
+++ GameBoard.js
@@ -6,4 +6,26 @@ class GameBoard {
         this.grid = [];
         this.DOMGrid = DOMGrid;
     }
+
+    showGameStatus(winGame) {
+        const div = document.createElement('div');
+        div.classList.add('game-status');
+        div.innerHTML = `${winGame ? "WINNER!" : "GAME OVER!"}`;
+        this.DOMGrid.appendChild(div);
+    }
+
+    createGrid(level) {
+        // wipe divs & grid upon start
+        this.dotCount = 0;
+        this.grid = [];
+        this.DOMGrid.innerHTML = '';
+        this.DOMGrid.style.cssText = `grid-template-columns: repeat(${GRID_SIZE}, ${CELL_SIZE}px)`;
+
+        level.forEach((square) => {
+            const div = document.createElement('div');
+            div.classList.add('square', CLASS_LIST[square]);
+            div.style.cssText = `width: ${CELL_SIZE}px; height: ${CELL_SIZE}px;`;
+            this.DOMGrid.appendChild(div);
+        })
+    }
 }
