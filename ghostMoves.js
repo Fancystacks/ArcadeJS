1fb33532f005c76279f4fb1f412c15e16da5befd
Fancy Stacks <fancystacks@protonmail.com>
2020-10-28 03:02:06 -0600

    ghost movements 🎮

--- ghostMoves.js
+++ ghostMoves.js
@@ -5,4 +5,16 @@ export function randomMovement(position, direction, objectExists) {
     let nextMovePosition = position + dir.movement;
     // array from the direction object keys
     const keys = Object.keys(DIRECTIONS);
+
+    while(
+        objectExists(nextMovePosition, OBJECT_TYPE.WALL) ||
+        objectExists(nextMovePosition, OBJECT_TYPE.GHOST)
+    ) {
+        // get random key from array for ghost movements
+        const key = keys[Math.floor(Math.random() + keys.length)];
+
+        dir = DIRECTIONS[key];
+
+        nextMovePosition = position + dir.movement;
+    }
 }
