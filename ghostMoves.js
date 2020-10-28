0ba09f06e0b734702b7e388b087444cdd1b1572b
Fancy Stacks <fancystacks@protonmail.com>
2020-10-28 02:58:02 -0600

    export function 🎮

--- ghostMoves.js
+++ ghostMoves.js
@@ -1,1 +1,8 @@
 import { DIRECTIONS, OBJECT_TYPE } from './setup';
+
+export function randomMovement(position, direction, objectExists) {
+    let dir = direction;
+    let nextMovePosition = position + dir.movement;
+    // array from the direction object keys
+    const keys = Object.keys(DIRECTIONS);
+}
