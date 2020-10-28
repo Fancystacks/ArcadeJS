54594c85ef8c53fc1860ecefa7b477f63569533c
Fancy Stacks <fancystacks@protonmail.com>
2020-10-28 02:49:58 -0600

    ghost constructor 🎮

--- Ghost.js
+++ Ghost.js
@@ -1,2 +1,11 @@
 import { DIRECTIONS, OBJECT_TYPE } from './setup';
 
+class Ghost{
+    constructor(speed, startPosition, movement, name) {
+       this.name = name;
+       this.movement = movement;
+       this.startPosition = startPosition;
+       this.position = startPosition;
+   }
+
+}
